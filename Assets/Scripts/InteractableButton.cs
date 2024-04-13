4ea4a36dfa5de3a48de7904b86c4708a628a80d7
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-04-14 00:04:40 +0400

    patch 01

--- Assets/Scripts/InteractableButton.cs
+++ Assets/Scripts/InteractableButton.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InteractableButton : MonoBehaviour, IPointerEnterHandler
+public class InteractableButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image _image;
     [SerializeField] private Color _onPointerEnterColor;
+    [SerializeField] private Color _onPointerExitColor;
 
     private void Start() =>
         _image = GetComponent<Image>();
 
     public void OnPointerEnter(PointerEventData eventData) =>
         _image.color = _onPointerEnterColor;
+
+    public void OnPointerExit(PointerEventData eventData) =>
+        _image.color = _onPointerExitColor;
 }
