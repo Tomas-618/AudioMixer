d92fe380269d9b4c7da0bdaf68f756ae1b5607f7
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-04-14 01:27:33 +0400

    patch 02

--- Assets/Scripts/InteractableButton.cs
+++ Assets/Scripts/InteractableButton.cs
@@ -1,19 +1,72 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InteractableButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+[RequireComponent(typeof(Image))]
+public class InteractableButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerClickHandler
 {
-    [SerializeField] private Image _image;
+    [SerializeField, Range(0, 1)] private float _deltaColorValue;
+
+    [SerializeField] private UnityEvent _onClick;
     [SerializeField] private Color _onPointerEnterColor;
     [SerializeField] private Color _onPointerExitColor;
+    [SerializeField] private Color _onPointerDownColor;
+
+    private Coroutine _coroutine;
+    private Image _image;
+
+    private void Reset()
+    {
+        _deltaColorValue = 0.05f;
+        _onPointerEnterColor = Color.white;
+        _onPointerExitColor = Color.white;
+        _onPointerDownColor = Color.white;
+    }
 
     private void Start() =>
         _image = GetComponent<Image>();
 
-    public void OnPointerEnter(PointerEventData eventData) =>
-        _image.color = _onPointerEnterColor;
+    private void OnMouseDown() =>
+        _onClick?.Invoke();
 
-    public void OnPointerExit(PointerEventData eventData) =>
-        _image.color = _onPointerExitColor;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeColor(_image.color, _onPointerEnterColor));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeColor(_image.color, _onPointerExitColor));
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeColor(_image.color, _onPointerDownColor));
+    }
+
+    public void OnPointerClick(PointerEventData eventData) =>
+        OnPointerEnter(eventData);
+
+    private IEnumerator ChangeColor(Color startColor, Color endColor)
+    {
+        float currentColorValue = 0;
+
+        while (_image.color != endColor)
+        {
+            _image.color = Color.Lerp(startColor, endColor, currentColorValue += _deltaColorValue);
+
+            yield return null;
+        }
+    }
 }
