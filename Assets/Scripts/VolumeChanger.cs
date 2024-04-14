a1bbc84522ec736f993fa16fccce46e349c8a2ba
Thomas2424 <113831322+Tomas-618@users.noreply.github.com>
2024-04-14 16:29:22 +0400

    patch 11

--- Assets/Scripts/VolumeChanger.cs
+++ Assets/Scripts/VolumeChanger.cs
@@ -6,6 +6,24 @@ public class VolumeChanger : MonoBehaviour
     [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private AudioMixerParameter _parameter;
 
-    public void OnChange(float value) =>
+    private float _volume;
+    private bool _isMuted;
+
+    public void OnChange(float value)
+    {
+        _volume = value;
+
+        if (_isMuted)
+            return;
+
         _audioMixer.SetFloat(_parameter.ToString(), value);
+    }
+
+    public void ChangeMutingState()
+    {
+        int minVolume = -80;
+
+        _audioMixer.SetFloat(_parameter.ToString(), _isMuted ? _volume : minVolume);
+        _isMuted = !_isMuted;
+    }
 }
